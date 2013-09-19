221afdbd05979471a35960b53783759f57d829a0
Stanislav Seletskiy <s.seletskiy@office.ngs.ru>
2013-09-19 13:54:54 +0700

    .zsh: git aliases + C-S to sudo

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -1,4 +1,21 @@
-alias S='sudo'
-alias S!='sudo `fc -ln -1`'
+unalias _
 
 alias gdc='git diff --cached'
+alias gss='git status -s'
+alias gl='git log --oneline --graph --decorate --all'
+alias gc='git commit -v'
+alias gcm='git commit -m'
+alias gc!='git commit --amend'
+alias gcm!='git commit --amend -m'
+alias gp='git push'
+alias gpo'git push origin'
+alias gr='git remote'
+alias grs='git remote set-url'
+alias gpl='git pull --rebase'
+
+zle -N prepend-sudo prepend_sudo
+bindkey "^S" prepend-sudo
+function prepend_sudo() {
+    BUFFER="sudo "$BUFFER
+    CURSOR=$(($CURSOR+5))
+}
