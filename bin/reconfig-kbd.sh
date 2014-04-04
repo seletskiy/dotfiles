4035ce47dba3a68578c2f9020ed57f7d82436c29
Stanislav Seletskiy <s.seletskiy@office.ngs.ru>
2014-04-04 17:33:21 +0700

    kbd: faster!

--- bin/reconfig-kbd.sh
+++ bin/reconfig-kbd.sh
@@ -3,6 +3,6 @@
 export XAUTHORITY=/home/s.seletskiy/.Xauthority
 export DISPLAY=:0.0
 
-xset r rate 190 70
+xset r rate 200 60
 setxkbmap -layout us,ru
 xmodmap -e 'keycode 66 = ISO_Next_Group ISO_Next_Group 0x0 0x0'
