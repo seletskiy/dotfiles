bcd9af825d5f6b394e59ff2d80af3e8e7f7d536f
Stanislav Seletskiy <s.seletskiy@gmail.com>
2014-06-06 17:09:16 +0700

    zsh: alt-. -> default, alt-, -> jump prev words

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -35,10 +35,22 @@ function prepend_sudo() {
     CURSOR=$(($CURSOR+5))
 }
 
-zle -N insert-prev-word insert_prev_word
-bindkey "\e." insert-prev-word
-function insert_prev_word() {
-    if (( _altdot_histno != HISTNO || _ilw_cursor != $CURSOR )); then
+autoload smart-insert-last-word
+bindkey "\e." smart-insert-last-word-wrapper
+bindkey "\e," smart-insert-prev-word
+zle -N smart-insert-last-word-wrapper
+zle -N smart-insert-prev-word
+function smart-insert-last-word-wrapper() {
+    _altdot_reset=1
+    smart-insert-last-word
+}
+function smart-insert-prev-word() {
+    if (( _altdot_reset )); then
+        _altdot_histno=$HISTNO
+        (( _altdot_line=-_ilw_count ))
+        _altdot_reset=0
+        _altdot_word=-2
+    elif (( _altdot_histno != HISTNO || _ilw_cursor != $CURSOR )); then
         _altdot_histno=$HISTNO
         _altdot_word=-1
         _altdot_line=-1
@@ -49,8 +61,8 @@ function insert_prev_word() {
     smart-insert-last-word $_altdot_line $_altdot_word 1
 
     if [[ $? -gt 0 ]]; then
-        _altdot_word=0
+        _altdot_word=-1
         _altdot_line=$((_altdot_line-1))
-        insert_prev_word
+        smart-insert-last-word $_altdot_line $_altdot_word 1
     fi
 }
