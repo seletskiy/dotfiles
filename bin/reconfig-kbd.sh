50e36eedb8a60fad4e16db2eab526d9c056176e4
Stanislav Seletskiy <s.seletskiy@gmail.com>
2014-06-06 12:57:40 +0700

    further keyboard tweaks
    * swap alt and win;
    * disable real arrows and home/end/pgup/pgdown;
    * remap home/pgup/pgdown/end to win+uiop;

--- bin/reconfig-kbd.sh
+++ bin/reconfig-kbd.sh
@@ -5,7 +5,9 @@ export DISPLAY=:0.0
 
 xset r rate 200 60
 setxkbmap \
+    -keycodes \
+        "evdev+aliases(qwerty)+disable-arrows" \
     -symbols \
-        "pc+us+ru:2+hjkl-ru+level3(lwin_switch)+mod4-lvl3+terminate(ctrl_alt_bksp)+capslock-groupshift" \
+        "pc+us+ru:2+hjkl-ru+level3(lwin_switch)+mod4-lvl3+terminate(ctrl_alt_bksp)+capslock-groupshift+altwin(swap_alt_win)" \
     -compat \
         "complete+hjkl"
