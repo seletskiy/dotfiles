fc4be8887045081afc9c04fd751f6a04c72c8a09
Stanislav Seletskiy <s.seletskiy@gmail.com>
2014-10-25 06:46:29 +0700

    vpn: fix internal routes

--- rootfs/etc/ppp/ip-up.d/01-gw.sh
+++ rootfs/etc/ppp/ip-up.d/01-gw.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [[ "$6" == "ngs" ]]; then
-   ip r a 192.168.0.0/16 dev ppp0
+   ip r a 192.168.22.0/24 dev ppp0
+   ip r a 192.168.21.0/24 dev ppp0
+   ip r a 192.168.20.0/24 dev ppp0
+   ip r a 192.168.2.0/24 dev ppp0
+   ip r a 192.168.4.0/24 dev ppp0
    ip r a 172.28.0.0/16 dev ppp0
    ip r a 172.16.0.0/16 dev ppp0
 fi
