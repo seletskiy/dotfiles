6b29ddd36acadd1ef67e02fb6190d2b18127bbb1
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-01-27 13:27:00 +0600

    zsh: fix infinit ssh

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -158,7 +158,7 @@ alias ssh='ssh-urxvt'
 function ssh-urxvt() {
     # in case of stdin, stdout or stderr is not a terminal, fallback to ssh
     if [[ ! ( -t 0 && -t 1 && -t 2 ) ]]; then
-        ssh "$@"
+        \ssh "$@"
     fi
 
     # if there more than one arg (hostname) without dash "-", fallback to ssh
@@ -166,12 +166,12 @@ function ssh-urxvt() {
     for arg in "$@"; do
         if [ ${arg:0:1} != - ]; then
             if [[ -n $hostname ]]; then
-                ssh "$@"
+                \ssh "$@"
             fi
             hostname=$arg
         fi
     done
 
     # check terminal is known, if not, fallback to xterm
-    ssh -t "$@" "infocmp >/dev/null 2>&1 || export TERM=xterm; \$SHELL"
+    \ssh -t "$@" "infocmp >/dev/null 2>&1 || export TERM=xterm; \$SHELL"
 }
