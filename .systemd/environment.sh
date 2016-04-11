3387c406a34ccfe2391af9e267aab32c9d13aad0
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-04-11 23:00:20 +0600

    migrate to systemd --user

--- .systemd/environment.sh
+++
@@ -1,5 +0,0 @@
-export DOTFILES_PROFILE=$(cat ~/.dotfiles/profile)
-export PATH=$(find -L $HOME/bin -type d -printf "%p:")$HOME/.go/bin/:$HOME/.deadfiles/bin:$PATH
-export GOPATH=~/.go/
-export EDITOR=vim
-export DMENU_PATH=~/bin/.dmenu/
